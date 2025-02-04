dc262ca4934c55896b73dbf8a720a49ce2d0ccc4
Fran Wu <fran.wu@huhtamaki.com>
2025-02-04 20:14:11 +0800

    2

--- Volo.Abp.Account.Web/AbpAccountUserMenuContributor.cs
+++ Volo.Abp.Account.Web/AbpAccountUserMenuContributor.cs
@@ -18,8 +18,8 @@ public class AbpAccountUserMenuContributor : IMenuContributor
 
         var uiResource = context.GetLocalizer<AbpUiResource>();
         var accountResource = context.GetLocalizer<AccountResource>();
-
-        context.Menu.AddItem(new ApplicationMenuItem("Account.Manage", accountResource["MyAccount"], url: "~/Account/Manage", icon: "fa fa-cog", order: 1000, null));
+        //removed by fran wu
+        //context.Menu.AddItem(new ApplicationMenuItem("Account.Manage", accountResource["MyAccount"], url: "~/Account/Manage", icon: "fa fa-cog", order: 1000, null));
         context.Menu.AddItem(new ApplicationMenuItem("Account.Logout", uiResource["Logout"], url: "~/Account/Logout", icon: "fa fa-power-off", order: int.MaxValue - 1000));
 
         return Task.CompletedTask;
